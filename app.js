6abeedc5b20f71c4ad4eb8747de28a3901420da6
idanlo <idan2110@gmail.com>
2018-06-16 14:23:10 +0300

    Add global chat with online users

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const flash = require("connect-flash");
 const Post = require("./models/Post");
 const User = require("./models/User");
 const port = process.env.port || 3000;
+const onlineChatUsers = [];
 
 const routes = require("./routes/routes");
 const app = express();
@@ -61,9 +62,31 @@ const server = app.listen(port, () => {
 const io = socket(server);
 
 io.on("connection", socket => {
-    console.log(socket.id);
+    console.log("new user ", socket.id);
+    // onlineChatUsers.push({
+    //     _id: req.user._id,
+
+    // });
+    io.sockets.emit("newUser", { socketID: socket.id });
+
+    socket.on("newUser", data => {
+        if (!onlineChatUsers.find(o => o.name === data.name)) {
+            onlineChatUsers.push(data);
+            io.sockets.emit("updateUserList", onlineChatUsers);
+            console.log(onlineChatUsers);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        let dcUser = onlineChatUsers.find(o => o.socketID === socket.id);
+        let index = onlineChatUsers.indexOf(dcUser);
+        onlineChatUsers.splice(index, 1);
+        io.sockets.emit("updateUserList", onlineChatUsers);
+        console.log(`user ${socket.id} disconnected`);
+    });
 
     socket.on("chat", data => {
+        console.log(data);
         io.sockets.emit("chat", data);
     });
 });
